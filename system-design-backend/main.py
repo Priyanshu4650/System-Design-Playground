202eeba9e1bb496cd9d71caf8b5c35e01e91a3c9
Priyansh4650 <priyanshu022017@gmail.com>
2025-12-27 15:06:34 +0530

    v0.1

---
+++ system-design-backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from .routes.health_routes import router as health_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(health_router, tags=["Health"])
+
